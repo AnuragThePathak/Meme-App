ad3b398790f2e60c51b321b604b6c38d33498a98
Anurag Pathak <anuragpathak911@gmail.com>
2021-09-05 17:20:40 +0530

    Added progressbar and next image function.

--- app/src/main/java/com/anurag/memeapp/MainActivity.kt
+++ app/src/main/java/com/anurag/memeapp/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.anurag.memeapp
 
+import android.graphics.drawable.Drawable
 import android.os.Bundle
+import android.view.View
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.android.volley.Request
@@ -8,6 +10,10 @@ import com.android.volley.toolbox.JsonObjectRequest
 import com.android.volley.toolbox.Volley
 import com.anurag.memeapp.databinding.ActivityMainBinding
 import com.bumptech.glide.Glide
+import com.bumptech.glide.load.DataSource
+import com.bumptech.glide.load.engine.GlideException
+import com.bumptech.glide.request.RequestListener
+import com.bumptech.glide.request.target.Target
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
@@ -23,24 +29,64 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun loadMeme() {
+        val progressBar = binding.progressBar
+        progressBar.visibility = View.VISIBLE
+//        progressBar.isVisible=true
+
+//        Creating the queue in MainActivity
         val queue = Volley.newRequestQueue(this)
         val url = "https://meme-api.herokuapp.com/gimme"
 
+//        Creating the request using JsonObjectRequest function.
         val jsonObjectRequest = JsonObjectRequest(
             Request.Method.GET, url, null,
+
             { response ->
                 val nameUrl = response.getString("url")
-                Glide.with(this).load(nameUrl).into(binding.imageView)
+
+//                Glide manages the image loading and caching.
+                Glide.with(this).load(nameUrl).listener(
+                    object : RequestListener<Drawable> {
+
+                        override fun onLoadFailed(
+                            e: GlideException?,
+                            model: Any?,
+                            target: Target<Drawable>?,
+                            isFirstResource: Boolean
+                        ): Boolean {
+                            progressBar.visibility = View.GONE
+                            return false
+                        }
+
+                        override fun onResourceReady(
+                            resource: Drawable?,
+                            model: Any?,
+                            target: Target<Drawable>?,
+                            dataSource: DataSource?,
+                            isFirstResource: Boolean
+                        ): Boolean {
+                            progressBar.visibility = View.GONE
+                            return false
+                        }
+                    }).into(binding.imageView)
             },
+
             {
-                Toast.makeText(this, "Something went wrong.", Toast.LENGTH_SHORT).show()
+                progressBar.visibility = View.GONE
+//                progressBar.isVisible = false
+
+                Toast.makeText(
+                    this, "Something went wrong.",
+                    Toast.LENGTH_SHORT
+                ).show()
             }
         )
 
-// Access the RequestQueue through your singleton class.
+// Add the request to queue.
         queue.add(jsonObjectRequest)
     }
 
-    fun nextMeme(view: android.view.View) {}
-    fun shareMeme(view: android.view.View) {}
+    fun nextMeme(view: View) = this.loadMeme()
+
+    fun shareMeme(view: View) {}
 }
